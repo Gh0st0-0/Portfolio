42b88bc6da6fc912c886527bfa534bfe97e4b526
Saumya Garg <saumyagwn98@gmail.com>
2023-06-08 17:53:11 +0530

    Saumya Component completed, integration with backend remaning

--- PortfolioFrontEnd/portfolio/src/components/Saumya.js
+++ PortfolioFrontEnd/portfolio/src/components/Saumya.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 export default function Saumya(){
 
     // const {candidate, setCandidate} = useContext(CandidateContext);
-    const {candidate, setCandidate} = useState(
+    const [candidate, setCandidate] = useState(
         {
             id: 1,
             userName: "Gh0st",
@@ -20,15 +20,33 @@ export default function Saumya(){
         }
     )
 
+    const [credentials, setCredentials] = useState(
+        {
+            id: 1,
+            primaryNo: "+91-9993522409",
+            secondryNo: "+91-9425410221",
+            email: "saumyagwl98@gmail.com",
+            linkedin: "https://www.linkedin.com/in/saumya-garg-693534173/",
+            hackerRank: "https://www.hackerrank.com/saumyagwl98?hr_r=1"
+        }
+    );
+
     async function fetchCandidate() {
         const {data} = await axios.get("http://localhost:8080/candidate/getcandidate/1");
         console.log(data);
         setCandidate(data);
     }
 
+    async function fetchCredentials(){
+        const {data} = await axios.get("http://localhost:8080/credentials/getcreds/1");
+        console.log(data);
+        setCredentials(data);
+    }
+
     // useEffect(
     //     () => {
     //         fetchCandidate();
+    //         fetchCredentials();
     //     }
     //     ,[]);
 
@@ -37,9 +55,39 @@ export default function Saumya(){
             <div>
                 <img src={saumya} alt="Saumya Garg" className={'saumyaImage'}/>
             </div>
-            <div>
-                <h2>Saumya Garg</h2>
-                {/* <h2>{candidate.firstName} {candidate.lastName}</h2> */}
+            <div className={'CandidateContainer'}>
+                <h2>{candidate.firstName} {candidate.lastName}</h2>
+                <h3>Current Working Location: {candidate.currentWorkingLocation}</h3>
+                <h3>As a: {candidate.roll}</h3>
+                <div>
+                    <p>
+                        Highly motivated and enthusiastic Full Stack Developer seeking an
+                        opportunity to apply my skills and knowledge in a professional setting.
+                        With a strong foundation in web development, I am eager to contribute
+                        to the success of a dynamic organization while continuously learning
+                        and growing as a developer.
+                    </p>
+                </div>
+            </div>
+            <div className={'CredentialContainer'}>
+                <div>
+                    Contact-No: 
+                    <p>
+                        {credentials.primaryNo}, 
+                        {credentials.secondryNo}
+                    </p>
+                </div>
+                <a href='#'>
+                    <i class='bx bxl-gmail bx-flashing' ></i> {credentials.email}
+                </a>
+                
+                <a href={credentials.linkedin}>
+                    <i class='bx bxl-linkedin bx-flashing' ></i> Linkedin
+                </a>
+
+                <a href={credentials.hackerRank}>
+                    <i class='bx bx-code bx-flashing' ></i> HackerRank
+                </a>
             </div>
         </div>
     )

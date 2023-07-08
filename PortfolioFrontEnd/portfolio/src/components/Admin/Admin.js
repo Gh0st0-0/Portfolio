ba6708ab0e24911fecce3372fc759e12819f0126
Saumya Garg <saumyagwn98@gmail.com>
2023-07-08 19:02:50 +0530

    Project state updated in the Admin component and ProjectContainer

--- PortfolioFrontEnd/portfolio/src/components/Admin/Admin.js
+++ PortfolioFrontEnd/portfolio/src/components/Admin/Admin.js
@@ -9,7 +9,8 @@ const AddProjectCard = lazy(() => import('./AddProjectCard'));
 
 export default function Admin() {
 
-    const [projects, setProjects] = useState([
+    const [projects, setProjects] = useState(
+    [
         {
             id: 1,
             projectTitle: "Advertisement Portal",
@@ -19,7 +20,7 @@ export default function Admin() {
             projectStartDate: "2023-02-20",
             projectEndDate: "2023-03-15",
             projectDuration: 4,
-            projectRole: "Project Lead and Fullstack Developer",
+            projectRole: "Project Lead and FUllstack Developer",
             responsibility: "Develope the Frontend and the backend and allocate tasks and manage progress",
             images: [
                 {
@@ -29,8 +30,8 @@ export default function Admin() {
                 },
                 {
                     id: 2,
-                    imageName: "RavenAdverts_LoginPage",
-                    imageURL: "https://ik.imagekit.io/vrchd4vczr/RavenAdverts/LoginPage.jpeg?updatedAt=1686480413054"
+                    imageName: "RavenAdverts_ClientPage",
+                    imageURL: "https://ik.imagekit.io/vrchd4vczr/RavenAdverts/CustomerLandingPageAfterAddingProduct.jpeg?updatedAt=1686480413140"
                 },
                 {
                     id: 3,
@@ -39,8 +40,8 @@ export default function Admin() {
                 },
                 {
                     id: 4,
-                    imageName: "RavenAdverts_ClientPage",
-                    imageURL: "https://ik.imagekit.io/vrchd4vczr/RavenAdverts/CustomerLandingPageAfterAddingProduct.jpeg?updatedAt=1686480413140"
+                    imageName: "RavenAdverts_LoginPage",
+                    imageURL: "https://ik.imagekit.io/vrchd4vczr/RavenAdverts/LoginPage.jpeg?updatedAt=1686480413054"
                 },
                 {
                     id: 5,
@@ -57,9 +58,58 @@ export default function Admin() {
                     imageName: "RavenAdverts_AdminPage",
                     imageURL: "https://ik.imagekit.io/vrchd4vczr/RavenAdverts/AdminCrudOfCustomer.jpeg?updatedAt=1686480413137"
                 }
-                
             ],
             technologiesUsed: "Java, J2EE, React JS, Node JS, JavaScript, CSS"
+        },
+        {
+            id: 2,
+            projectTitle: "Delhi Metro Phase 4",
+            affeleatedCompany: "Techture Pvt. Ltd.",
+            discription: "Techture is responsible for the comprehensive BIM modeling and 4D/5D simulation of Delhi Metro's Phase 4 project. They have also stationed a BIM coordinator on-site to enhance communication and project execution, aiming to improve last-mile connectivity in the NCR housing hubs.",
+            releventLinks: "https://www.techture.global/projects/delhi-metro-phase-4",
+            projectStartDate: "2019-03-07",
+            projectEndDate: "2022-05-01",
+            projectDuration: 164,
+            projectRole: "Coordinated Drawing Drafter and QA",
+            responsibility: "Generation of coordinated drawings for LOD 300 and LOD 400 Models.",
+            images: [
+                {
+                    id: 10,
+                    imageName: "Techture_Logo",
+                    imageURL: "https://ik.imagekit.io/vrchd4vczr/DMRC-Phase-4/Techture_Logo.jpg?updatedAt=1688732534525"
+                },
+                {
+                    id: 11,
+                    imageName: "DMRC_Cover",
+                    imageURL: "https://ik.imagekit.io/vrchd4vczr/DMRC-Phase-4/Cover_DMRC.jpg?updatedAt=1688732534548"
+                },
+                {
+                    id: 12,
+                    imageName: "DMRC_Magenta_Line",
+                    imageURL: "https://ik.imagekit.io/vrchd4vczr/DMRC-Phase-4/DMRC_Magenta_Line.jpg?updatedAt=1688732534275"
+                },
+                {
+                    id: 13,
+                    imageName: "DMRC_Pile_Reference_Drawing",
+                    imageURL: "https://ik.imagekit.io/vrchd4vczr/DMRC-Phase-4/DMRC_Pile.jpg?updatedAt=1688733012349"
+                },
+                {
+                    id: 14,
+                    imageName: "DMRC_Pile_Reference",
+                    imageURL: "https://ik.imagekit.io/vrchd4vczr/DMRC-Phase-4/DMRC_PileRef.jpg?updatedAt=1688734556274"
+                },
+                {
+                    id: 15,
+                    imageName: "DMRC_Pile_Plinth_Reference_Drawing",
+                    imageURL: "https://ik.imagekit.io/vrchd4vczr/DMRC-Phase-4/DMRC_Rebar_Drawing-2.jpg?updatedAt=1688734226927"
+                },
+                {
+                    id: 16,
+                    imageName: "DMRC_Pile_Drawing",
+                    imageURL: "https://ik.imagekit.io/vrchd4vczr/DMRC-Phase-4/Pile-drawing.jpg?updatedAt=1688734873043"
+                }
+            ],
+            technologiesUsed: "BIM, Infrastructure, Revit, AutoCAD, Naviswork"
         }
     ]);
 

e80551fa938aad34512258af0f5bb71f1ff53c8b
Gh0st0-0 <einwahnsinnige@gmail.com>
2023-11-25 16:25:27 +0530

    Finished Experience Card

--- PortfolioFrontEnd/portfolio/src/components/Experience/ExperienceCard.js
+++ PortfolioFrontEnd/portfolio/src/components/Experience/ExperienceCard.js
@@ -4,7 +4,28 @@ const ExperienceCard = ({exper}) => {
     return (
         <div className={'ExperienceCard'}>
             <div>
-                <h1>{exper.companyName}</h1>  <h1>: {exper.companyLocation}</h1>
+                <img src={exper && exper.companyLogo} loading={'lazy'} alt={exper.companyName + " Logo"}/>
+            </div>
+            <div className = {'CompanyInfo'}>
+                <div>
+                    <h1>{exper.companyName}</h1>  <h1>: {exper.companyLocation}</h1>
+                </div>
+                <div>
+                    <h2>Designation: {exper.companyDesignation}</h2>
+                </div>
+                <div>
+                    <h2>Date From: {exper.dateFrom}</h2>
+                </div>
+                {exper.dateTo && <div>
+                    <h2>Date To: {exper.dateTo}</h2>
+                </div>}
+                {exper.dateTo != null ? <div>
+                    <h2>Period Dedicated: {exper.timeServed} Months</h2> 
+                </div> : 
+                <div>
+                    <h2>Period Dedicating: {exper.timeServed} Months</h2>
+                </div>
+                }
             </div>
         </div>
     )

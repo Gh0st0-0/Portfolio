ceffc15e32801c31efd15558df9be2f97ddc1368
Gh0st0-0 <einwahnsinnige@gmail.com>
2023-12-25 17:23:34 +0530

    Admin add experience

--- PortfolioFrontEnd/portfolio/src/components/Admin/Experience/AddExperience.js
+++ PortfolioFrontEnd/portfolio/src/components/Admin/Experience/AddExperience.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {useNavigate} from "react-router-dom";
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {toast} from 'react-toastify';
 
 export default function AddExperience(){
@@ -8,13 +8,13 @@ export default function AddExperience(){
 
     const [experience, setExperience] = useState(
         {
-            companyName: "Techture",
-            companyLogo: "https://ik.imagekit.io/vrchd4vczr/CompanyLogos/techture.jfif?updatedAt=1700908305341",
-            companyLocation: "Indore M.P.",
-            dateFrom: "2020-01-30",
-            dateTo: "2021-04-02",
-            companyDesignation: "BIM Intern",
-            timeServed: 15
+            companyName: "",
+            companyLogo: "",
+            companyLocation: "",
+            dateFrom: "",
+            dateTo: "",
+            companyDesignation: "",
+            timeServed: 0
         }
     );
 
@@ -23,8 +23,33 @@ export default function AddExperience(){
         setExperience({...experience, [name]: value})
     }
 
+    const calculateTime = useCallback(() => {
+        console.log(experience.dateTo);
+    
+        const startDate = new Date(experience.dateFrom);
+        const endDate = new Date(experience.dateTo);
+
+        const months = (endDate.getFullYear() - startDate.getFullYear()) * 12 + endDate.getMonth() - startDate.getMonth();
+
+        setExperience(prevExperience => ({
+            ...prevExperience,
+            timeServed: months
+        }));
+    
+    }, [experience.dateFrom, experience.dateTo]);
+
+    useEffect(() => {
+        calculateTime();
+    }, [calculateTime]);
+
     async function persistExperience() {
         try{
+            if(experience.dateTo === "" || experience.dateTo === null) {
+                setExperience(prevExperience => ({
+                    ...prevExperience,
+                    dateTo: null
+                }));
+            }
             const {data} = await axios.post("http://localhost:8080/experience/persist/saveExperience", experience)
             if(data){
                 notify();
@@ -73,12 +98,56 @@ export default function AddExperience(){
             <h2>
                 Add Experience Record
             </h2>
-            <div className={'Admin-Add-Exper-form col-md-4'}>
+            <div className={'Admin-Add-Exper-form col-md-6'}>
                 <div className={'row'}>
-                    <div></div>
+                    <div className={'group col-md-6'}>
+                        <label htmlFor={'companyName'}>Company Name</label>
+                        <input type={'text'} className={'form-control'} name={'companyName'} id={'companyName'} value={experience.companyName} onChange={HandleChanges} />
+                    </div>
+                    <div className={'group col-md-3'}>
+                        <label htmlFor={'companyLocation'}>Company Location</label>
+                        <input type={'text'} className={'form-control'} name={'companyLocation'} id={'companyLocation'} value={experience.companyLocation} onChange={HandleChanges} />
+                    </div>
+                    <div className={'group col-md-3'}>
+                        <label htmlFor={'companyDesignation'}>Company Designation</label>
+                        <input type={'text'} className={'form-control'} name={'companyDesignation'} id={'companyDesignation'} value={experience.companyDesignation} onChange={HandleChanges} />
+                    </div>
                 </div>
+                <div className={'row'}>
+                    <div className={'group col-md-4'}>
+                        <label htmlFor={'dateFrom'}>Date From</label>
+                        <input type={'date'} className={'form-control'} name={'dateFrom'} id={'dateFrom'} value={experience.dateFrom} onChange={HandleChanges} />
+                    </div>
+                    <div className={'group col-md-4'}>
+                        <label htmlFor={'dateTo'}>Date To</label>
+                        <input type={'date'} className={'form-control'} name={'dateTo'} id={'dateTo'} value={experience.dateTo} onChange={HandleChanges} />
+                    </div>
+                    <div className={'group col-md-4'}>
+                        <label htmlFor={'timeServed'}>Time Served</label>
+                        <input type={'number'} className={'form-control'} name={'timeServed'} id={'timeServed'} value={experience.timeServed} onChange={HandleChanges} />
+                    </div>
+                </div>
+                <div className={'row'}>
+                    <div className={'group'}>
+                        <lable htmlFor={'companyLogo'} >Company Logo Link</lable>
+                        <input type={'text'} className={'form-control'} name={'companyLogo'} id={'companyLogo'} value={experience.companyLogo} onChange={HandleChanges} />
+                    </div>
+                </div>
+                {experience.companyLogo !== "" && <div className={'Form-Image'}>
+                    <img loading={'lazy'} src={experience.companyLogo} alt={experience.companyName + "Logo"} />
+                </div>}
+                <button className={'custom-button'} onClick={persistExperience}>Add Experience</button>
             </div>
         </div>
     )
 
 }
+// {
+        //     companyName: "Techture",
+        //     companyLogo: "https://ik.imagekit.io/vrchd4vczr/CompanyLogos/techture.jfif?updatedAt=1700908305341",
+        //     companyLocation: "Indore M.P.",
+        //     dateFrom: "2020-01-30",
+        //     dateTo: "2021-04-02",
+        //     companyDesignation: "BIM Intern",
+        //     timeServed: 15
+        // }
